149d23058af85921432dc6bc25130bfcea10cf2b
conqryash007 <guptayash200010@gmail.com>
2021-12-01 15:11:24 +0530

    added login and get fresher route

--- backend/routes/freshers-routes.js
+++ backend/routes/freshers-routes.js
@@ -4,6 +4,7 @@ const router = express.Router();
 const freshersContoller = require("../controllers/freshers-controller");
 
 router.get("/", freshersContoller.getFreshers);
+router.get("/:uid", freshersContoller.getFreshersById);
 
 router.post("/addFresher", freshersContoller.addFresher);
 
